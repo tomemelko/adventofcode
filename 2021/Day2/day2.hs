5e29397b65028c9060fbad0a5ff73923adcbfdda
Tom Emelko <tom.emelko@gmail.com>
2021-12-02 18:24:52 -0500

    Same thing but for Day 2

--- 2021/Day2/day2.hs
+++ 2021/Day2/day2.hs
@@ -1,14 +1,14 @@
 import Util
 
 parse_input :: String -> [[String]]
-parse_input in_str = map words (lines in_str)
+parse_input in_str = map words $ lines in_str
 
 sum_direction :: [[String]] -> String -> Integer
-sum_direction list direction = foldl (+) 0 (map (\n -> parse_int (n !! 1)) (filter (\n -> (n !! 0) == direction) list))
+sum_direction list direction = foldl (+) 0 $ map (\n -> parse_int (n !! 1)) $ filter (\n -> (n !! 0) == direction) list
 
 main = do
   in_str <- read_input "input.txt"
   let input = parse_input in_str
   let curried = sum_direction input
   -- Part 1
-  print (format_result_output 1 ((curried "forward") * ((curried "down") - (curried "up"))))
+  print_part_result 1 $ (curried "forward") * ((curried "down") - (curried "up"))
