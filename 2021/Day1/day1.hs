6b3a4e73d33abb0281a43d45523c12a21988c4c2
Tom Emelko <tom.emelko@gmail.com>
2021-12-02 18:22:51 -0500

    Haskell-erificify Day 1

--- 2021/Day1/day1.hs
+++ 2021/Day1/day1.hs
@@ -1,23 +1,24 @@
 import Util
 
-parse_input :: String -> []Integer
-parse_input s = map parse_int (lines s)
+parse_input :: String -> [Integer]
+parse_input s = map parse_int $ lines s
 
 is_greater :: Integer -> Integer -> Integer
-is_greater x y = if y > x then 1 else 0
+is_greater x y
+  | y > x     = 1
+  | otherwise = 0
 
 count_increases :: [Integer] -> Integer
-count_increases x = foldl (+) 0 (zipWith is_greater x (tail x))
+count_increases x = foldl (+) 0 $ zipWith is_greater x $ tail x
 
 sliding_window_sum :: Integer -> [Integer] -> [Integer]
-sliding_window_sum windowSize list = if windowSize == 1 then list else zipWith (+) list (sliding_window_sum (windowSize - 1) (tail list))
+sliding_window_sum windowSize list
+  | windowSize == 1 = list
+  | otherwise       = zipWith (+) list $ sliding_window_sum (windowSize - 1) $ tail list
 
 main = do
   in_str <- read_input "input.txt"
-  let input = parse_input in_str
   -- Part 1
-  let result1 = count_increases input
-  print (format_result_output 1 result1)
+  print_part_result 1 $ count_increases $ parse_input in_str
   -- Part 2
-  let result2 = count_increases (sliding_window_sum 3 input)
-  print (format_result_output 2 result2)
+  print_part_result 2 $ count_increases $ sliding_window_sum 3 $ parse_input in_str
--- 2021/Util.hs
+++ 2021/Util.hs
@@ -2,6 +2,7 @@ module Util (
   read_input
 , parse_int
 , format_result_output
+, print_part_result
 ) where
 
 read_input :: String -> IO String
@@ -12,3 +13,6 @@ parse_int s = read s :: Integer
 
 format_result_output :: Integer -> Integer -> String
 format_result_output partNum resultVal = "Part " ++ (show partNum) ++ " result: " ++ (show resultVal)
+
+print_part_result :: Integer -> Integer -> IO ()
+print_part_result partNum resultVal = print $ format_result_output partNum resultVal
