7a99dac6fe557ca62a7c3c16c083c065e739de0a
Tom Emelko <tom.emelko@gmail.com>
2021-12-02 23:18:05 -0500

    Do Day 2 Part 2

--- 2021/Day2/day2.hs
+++ 2021/Day2/day2.hs
@@ -1,10 +1,22 @@
 import Util
 
-parse_input :: String -> [[String]]
-parse_input in_str = map words $ lines in_str
+type SubmarineCommand = (String, String)
+data SubmarinePosition = SubmarinePosition { lateral :: Integer, depth :: Integer, aim :: Integer }
 
-sum_direction :: [[String]] -> String -> Integer
-sum_direction list direction = foldl (+) 0 $ map (\n -> parse_int (n !! 1)) $ filter (\n -> (n !! 0) == direction) list
+parse_input :: String -> [SubmarineCommand]
+parse_input in_str = map (\n -> (n !! 0, n !! 1)) $ map words $ lines in_str
+
+sum_direction :: [SubmarineCommand] -> String -> Integer
+sum_direction list direction = foldl (+) 0 $ map (\n -> parse_int (snd n)) $ filter (\n -> (fst n) == direction) list
+
+step :: SubmarinePosition -> SubmarineCommand -> SubmarinePosition
+step pos cmd
+  | (fst cmd) == "forward" = SubmarinePosition {lateral = (lateral pos) + (read $ snd cmd), depth = (depth pos) + ((aim pos) * (read $ snd cmd)), aim = (aim pos)}
+  | (fst cmd) == "down"    = SubmarinePosition {lateral = (lateral pos), depth = (depth pos), aim = (aim pos) + (read $ snd cmd)}
+  | (fst cmd) == "up"      = SubmarinePosition {lateral = (lateral pos), depth = (depth pos), aim = (aim pos) - (read $ snd cmd)}
+
+calc_aim_and_depth :: [SubmarineCommand] -> SubmarinePosition
+calc_aim_and_depth list = foldl step (SubmarinePosition {lateral = 0, depth = 0, aim = 0}) list
 
 main = do
   in_str <- read_input "input.txt"
@@ -12,3 +24,6 @@ main = do
   let curried = sum_direction input
   -- Part 1
   print_part_result 1 $ (curried "forward") * ((curried "down") - (curried "up"))
+  -- Part 2
+  let result2 = calc_aim_and_depth input
+  print_part_result 2 $ ((depth result2) * (lateral result2))
