1b940274ecc7ec32c87f2fde1bd48ab0f5726528
Tom Emelko <tom.emelko@gmail.com>
2021-12-04 16:31:48 -0500

    Refactor main to take args

--- 2021/app/Main.hs
+++ 2021/app/Main.hs
@@ -1,8 +1,18 @@
 module Main where
 
+import System.Environment
+
 import qualified Day1
 import qualified Day2
 import qualified Day3
 
 main :: IO ()
-main = Day1.showDay "inputs/Day1/input.txt"
+main = do
+  args <- getArgs
+  case args of 
+    [] -> print "Arg required: can be one of 'all', 'today easy', or 'today hard'"
+    ["all"] -> do 
+      Day1.showDay "inputs/Day1/input.txt"
+      Day2.showDay "inputs/Day2/input.txt"
+      Day3.showDay "inputs/Day3/input.txt"
+    
