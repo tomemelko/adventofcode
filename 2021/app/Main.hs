3dfb80e6ea9e76a78bd92bb5c9b9221700ad697b
Tom Emelko <tom.emelko@gmail.com>
2021-12-04 20:53:45 -0500

    Do Day 4 Part 1

--- 2021/app/Main.hs
+++ 2021/app/Main.hs
@@ -5,6 +5,7 @@ import System.Environment
 import qualified Day1
 import qualified Day2
 import qualified Day3
+import qualified Day4
 
 main :: IO ()
 main = do
@@ -15,4 +16,6 @@ main = do
       Day1.showDay "inputs/Day1/input.txt"
       Day2.showDay "inputs/Day2/input.txt"
       Day3.showDay "inputs/Day3/input.txt"
+    ["today", "easy"] -> Day4.showDay "inputs/Day4/input_simple.txt"
+    ["today", "hard"] -> Day4.showDay "inputs/Day4/input.txt"
     
--- 2021/src/Util.hs
+++ 2021/src/Util.hs
@@ -6,14 +6,25 @@ module Util (
 , split
 ) where
 
+import Data.Maybe
+import Text.Read
+import GHC.Stack
+
 readInput :: String -> IO String
 readInput = readFile
 
-parseInt :: Read a => String -> a
-parseInt = read
+parseInt :: HasCallStack => Read a => String -> a
+parseInt = fromJust . readMaybe
 
 formatResultOutput :: (Show a) => Integer -> a -> String
 formatResultOutput partNum resultVal = "Part " ++ show partNum ++ " result: " ++ show resultVal
 
 printPartResult :: (Show a) => Integer -> a -> IO ()
 printPartResult partNum resultVal = print $ formatResultOutput partNum resultVal
+
+-- This is a modified `words` that's more generic, and therefore can be used to split lists of strings on strings in addition to strings on chars
+split :: (a -> Bool) -> [a] -> [[a]]
+split p s = case dropWhile p s of
+  [] -> []
+  s' -> w : split p s''
+    where (w, s'') = break p s'
