223d44677bf82af6546fd935d6879cf34f68b9aa
Tom Emelko <tom.emelko@gmail.com>
2021-12-06 00:28:19 -0500

    Update Day 5 to be about 20x faster
    Before this I was considering all possible points and determining if they were on a line
    Afterwards build the list of points directly from the lines by iterating from one point to the other
    
    Haskell profiling tool helped me find the slowness :)

--- 2021/src/Day5.hs
+++ 2021/src/Day5.hs
@@ -1,8 +1,10 @@
 module Day5 where
 
+import qualified Data.Map as Map
+
 import Util
 
-data Point = Point { x :: Int, y :: Int }
+data Point = Point { x :: Int, y :: Int } deriving (Eq, Ord)
 instance Show Point where
   show p = "(" ++ (show . x) p ++ "," ++ (show . y) p ++ ")"
 type Line = (Point, Point)
@@ -16,44 +18,40 @@ makeLine s = ((makePoint . (!! 0) . words) s, (makePoint . (!! 2) . words) s)
 parseInput :: String -> [Line]
 parseInput = map makeLine . lines
 
-getPoints :: [Line] -> [Point]
-getPoints []       = []
-getPoints (l : ls) = fst l : snd l : getPoints ls
+unit :: Int -> Int
+unit 0 = 0
+unit x = x `div` abs x
 
-getPointRange :: [Point] -> (Int, Int)
-getPointRange ps = (maxBy x ps, maxBy y ps)
-  where maxBy f = maximum . map f
+(+:) :: Point -> Point -> Point
+m +: n = Point {x = x m + x n, y = y m + y n}
 
-getAllPointsInRange :: Int -> Int -> [Point]
-getAllPointsInRange maxX maxY = cartProd [0..maxX] [0..maxY] (\ x y -> Point {x = x, y = y})
+getDirectionOfLine :: Line -> Point
+getDirectionOfLine (p1, p2) = Point {x = unit (x p2 - x p1), y = unit (y p2 - y p1)}
 
-getAllPoints :: [Line] -> [Point]
-getAllPoints = uncurry getAllPointsInRange . getPointRange . getPoints
+getPointsOnLine :: Line -> [Point]
+getPointsOnLine (m, n) = go (getDirectionOfLine (m, n)) m
+  where
+    go :: Point -> Point -> [Point]
+    go direction x
+      | x == n    = [n]
+      | otherwise = x : go direction (x +: direction)
+
+countRepeats :: Ord a => [a] -> [(a, Int)]
+countRepeats = go Map.empty
+  where
+    go :: Ord a => Map.Map a Int -> [a] -> [(a, Int)]
+    go m xs
+      | null xs = Map.toList m
+      | otherwise = go (Map.insertWith (+) (head xs) 1 m) (tail xs)
 
 filterDiagonalLines :: [Line] -> [Line]
 filterDiagonalLines = filter (\l -> (x . fst) l == (x . snd) l || (y . fst) l == (y . snd) l)
 
-crossProduct :: Point -> Point -> Point -> Int
-crossProduct a b c = (y c - y a) * (x b - x a) - (x c - x a) * (y b - y a)
-
-squaredPointDistance :: Point -> Point -> Int
-squaredPointDistance a b = ((x b - x a) ^ 2) + ((y b - y a) ^ 2)
-
--- A point is a member of a line segment iff crossproduct is 0 AND the the distance between the ends of the segments is greater than the sum of each end to the given point
-member :: Point -> Line -> Bool
-member p l = uncurry crossProduct l p == 0 && uncurry squaredPointDistance l >= squaredPointDistance p (fst l) + squaredPointDistance p (snd l)
-
-cartProd :: [a] -> [b] -> (a -> b -> c) -> [c]
-cartProd xs ys f = [f x y | x <- xs, y <- ys]
-
-countLinesOnPoint :: Point -> [Line] -> Int
-countLinesOnPoint p = length . filter (member p)
-
 calcPart1 :: [Line] -> Int
-calcPart1 ls = (length . filter (>=2) . map (`countLinesOnPoint` filterDiagonalLines ls) . getAllPoints) ls
+calcPart1 = length . filter ((>1) . snd) . countRepeats . concatMap getPointsOnLine . filterDiagonalLines
 
 calcPart2 :: [Line] -> Int
-calcPart2 ls = (length . filter (>=2) . map (`countLinesOnPoint` ls) . getAllPoints) ls
+calcPart2 = length . filter ((>1) . snd) . countRepeats . concatMap getPointsOnLine
 
 showDay :: (Integer -> Int -> IO ()) -> String -> IO ()
 showDay printPartResult filename = do
