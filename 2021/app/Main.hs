97fb9bdfef91bad1109a0c6c3156f30fb8b96c3d
Tom Emelko <tom.emelko@gmail.com>
2021-12-12 21:28:30 -0500

    Do Day 9 Part 2

--- 2021/app/Main.hs
+++ 2021/app/Main.hs
@@ -28,6 +28,7 @@ main = do
       Day6.showDay (printDayResult 6) "inputs/Day6/input.txt"
       Day7.showDay (printDayResult 7) "inputs/Day7/input.txt"
       Day8.showDay (printDayResult 8) "inputs/Day8/input.txt"
+      Day9.showDay (printDayResult 9) "inputs/Day9/input.txt"
     ["today", "easy"] -> Day9.showDay (printDayResult 9) "inputs/Day9/input_simple.txt"
     ["today", "hard"] -> Day9.showDay (printDayResult 9) "inputs/Day9/input.txt"
     
--- 2021/src/Day9.hs
+++ 2021/src/Day9.hs
@@ -2,15 +2,23 @@ module Day9 where
 
 import Util
 import Data.Map (Map)
-import qualified Data.Map as Map
-import qualified Data.Bifunctor
 import Data.Maybe
+import qualified Data.Bifunctor
+import qualified Data.List as List
+import qualified Data.Map as Map
+import qualified Data.Set as Set
 
 type Point = (Int, Int)
 type Grid = Map Point Int
 
 get :: Grid -> Point -> Int
 get g p = fromJust (Map.lookup p g)
+
+setHead :: Set.Set a -> a
+setHead = head . Set.toList . Set.take 1
+
+setTail :: Set.Set a -> Set.Set a
+setTail = Set.drop 1
 
 parseInput :: String -> Grid
 parseInput = Map.fromList . map (Data.Bifunctor.second charToInt) . concatMap (\(y, s) -> zipWith (\a b -> ((a, y), b)) [0..] s) . zip [0..] . lines
@@ -23,7 +31,7 @@ maxPt g = (maxBy fst (Map.keys g), maxBy snd (Map.keys g))
 
 adjacentPoints :: Point -> Point -> [Point]
 adjacentPoints (maxX, maxY) (x,y) = [(x+m, y+n) | m <- [-1,0,1], n <- [-1,0,1],
-    (\(m', n') -> (m' == 0 && n' /= 0) || (m' /= 0 && n' == 0)) (m,n),
+    (m == 0 && n /= 0) || (m /= 0 && n == 0), -- Only non-diagonal adjacency
     x + m >= 0,
     y + n >= 0,
     x + m <= maxX,
@@ -36,10 +44,25 @@ allNeighborsGreaterThanPoint g p = all ((> get g p) . get g) (adjacentPoints (ma
 getLowPoints :: Grid -> [Point]
 getLowPoints g = filter (allNeighborsGreaterThanPoint g) (Map.keys g)
 
+getBasinSize :: Grid -> Point -> Int
+getBasinSize g lowPoint = go (Set.fromList [lowPoint]) Set.empty where
+  go :: Set.Set Point -> Set.Set Point -> Int
+  go toProcess visited
+   | Set.null toProcess = (length . filter (\x -> get g x < 9) . Set.toList) visited
+   | otherwise = processPoint (setHead toProcess) where
+     processPoint :: Point -> Int
+     processPoint p = go (Set.union (setTail toProcess) createNewToProcess) (Set.insert p visited) where
+       createNewToProcess :: Set.Set Point
+       createNewToProcess = Set.fromList (filter (\x -> x `Set.notMember` visited && get g p < 9) (adjacentPoints (maxPt g) p))
+
 calcPart1 :: Grid -> Int
 calcPart1 g = (sum . map ((+1) . get g)) (getLowPoints g)
+
+calcPart2 :: Grid -> Int
+calcPart2 g = (product . take 3 . reverse . List.sort . map (getBasinSize g) . getLowPoints) g
 
 showDay :: (Integer -> Int -> IO ()) -> String -> IO ()
 showDay printPartResult filename = do
   inStr <- readInput filename
   printPartResult 1 $ (calcPart1 . parseInput) inStr
+  printPartResult 2 $ (calcPart2 . parseInput) inStr
