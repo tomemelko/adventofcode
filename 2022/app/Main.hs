9857d246ebf459b4a7d7abc8be3d328898b3409c
Tom Emelko <tom.emelko@gmail.com>
2022-12-02 05:02:38 +0000

    Golf down to one liners for day 1

--- 2022/app/Main.hs
+++ 2022/app/Main.hs
@@ -13,7 +13,7 @@ main = do
     ["all"] -> do
       -- I really wish there was a way to reference these by string name so I didn't have to copy-paste these lines. Template Haskell looks scary :(
       Day1.showDay (printDayResult 1) "inputs/Day1/input.txt"
-    ["today", "easy"] -> Day1.showDay (printDayResult 17) "inputs/Day1/input_simple.txt"
-    ["today", "hard"] -> Day1.showDay (printDayResult 17) "inputs/Day1/input.txt"
+    ["today", "easy"] -> Day1.showDay (printDayResult 1) "inputs/Day1/input_simple.txt"
+    ["today", "hard"] -> Day1.showDay (printDayResult 1) "inputs/Day1/input.txt"
     _ -> do print "Invalid command"
     
--- 2022/src/Day1.hs
+++ 2022/src/Day1.hs
@@ -16,6 +16,13 @@ calcPart1 = maximum . sumParts
 calcPart2 :: [[Integer]] -> Integer
 calcPart2 = sum . take 3 . sortBy (flip compare) . sumParts
 
+-- Golfing!
+calcPart1Alt :: String -> Integer
+calcPart1Alt s = maximum . map (sum . (map parseInt . splitOn "\n")) $ splitOn "\n\n" s
+
+calcPart2Alt :: String -> Integer
+calcPart2Alt s = sum . take 3 . sortBy (flip compare) . map (sum . (map parseInt . splitOn "\n")) $ splitOn "\n\n" s
+
 showDay :: (Integer -> Integer -> IO ()) -> String -> IO ()
 showDay printPartResult filename = do
   in_str <- readInput filename
@@ -23,3 +30,7 @@ showDay printPartResult filename = do
   printPartResult 1 $ (calcPart1 . parseInput) in_str
   -- Part 2
   printPartResult 2 $ (calcPart2 . parseInput) in_str
+  -- Part 1
+  printPartResult 1 $ calcPart1Alt in_str
+  -- Part 2
+  printPartResult 2 $ calcPart2Alt in_str
