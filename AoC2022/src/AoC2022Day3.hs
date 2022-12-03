4efb5f90c1d798fce3e6be3971db1140722cb0d9
Tom Emelko <tom.emelko@gmail.com>
2022-12-03 06:41:13 +0000

    Solve day 3 part 2

--- AoC2022/src/AoC2022Day3.hs
+++ AoC2022/src/AoC2022Day3.hs
@@ -2,6 +2,7 @@ module AoC2022Day3( showDay ) where
 
 import Util
 import Data.Char
+import Data.List.Split
 
 parseInput :: String -> [String]
 parseInput = lines
@@ -9,8 +10,14 @@ parseInput = lines
 splitInHalf :: String -> (String, String)
 splitInHalf s = splitAt (length s `div` 2) s
 
-intersect :: (String, String) -> Char
-intersect (xs, ys) = (head . filter (`elem` xs)) ys
+intersect :: String -> String -> String
+intersect xs = filter (`elem` xs)
+
+intersectTuple :: (String, String) -> Char
+intersectTuple (xs, ys) = head (xs `intersect` ys)
+
+intersectList :: [String] -> String
+intersectList = foldl1 intersect
 
 score :: Char -> Integer
 score c
@@ -19,7 +26,10 @@ score c
   | otherwise = -1000000000
 
 calcPart1 :: [String] -> Integer
-calcPart1 = sum . map (score . intersect . splitInHalf)
+calcPart1 = sum . map (score . intersectTuple . splitInHalf)
+
+calcPart2 :: [String] -> Integer
+calcPart2 = sum . map (score . head . intersectList) . chunksOf 3
 
 showDay :: (Integer -> Integer -> IO ()) -> String -> IO ()
 showDay printPartResult filename = do
@@ -27,4 +37,4 @@ showDay printPartResult filename = do
   -- Part 1
   printPartResult 1 $ (calcPart1 . parseInput) in_str
   -- Part 2
-  -- printPartResult 2 $ (calcPart2 . parseInput) in_str
+  printPartResult 2 $ (calcPart2 . parseInput) in_str
