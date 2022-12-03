a89dd5d061a7520406efdf566ca1c99120cce637
Tom Emelko <tom.emelko@gmail.com>
2022-12-03 05:35:39 +0000

    Fix oopsie

--- newday.sh
+++ newday.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-mkdir .AoC$2/inputs/Day$1/
-touch .AoC$2/inputs/Day$1/input.txt ./inputs/Day$1/input_simple.txt src/Day$1.hs
+mkdir ./AoC$2/inputs/Day$1/
+touch ./AoC$2/inputs/Day$1/input.txt ./AoC$2/inputs/Day$1/input_simple.txt ./AoC$2/src/AoC$2Day$1.hs
