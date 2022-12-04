b0faf23887adec59e24098960fb95390122bb078
Tom Emelko <tom.emelko@gmail.com>
2022-12-04 05:40:21 +0000

    Solve day 4 with one-liners
    Modulo a few helpers

--- AoC2022/src/AoC2022Day4.hs
+++ AoC2022/src/AoC2022Day4.hs
@@ -24,6 +24,13 @@ calcPart1 = toInteger . length . filter checkPair
 calcPart2 :: [([Integer], [Integer])] -> Integer
 calcPart2 = toInteger . length . filter checkPairPt2
 
+-- One-liner-ing
+calcPart1Alt :: String -> Integer
+calcPart1Alt = toInteger . length . filter (\(as, bs) -> all (`elem` bs) as || all (`elem` as) bs) . map ((\l -> (head l, l !! 1)) . map ((\l -> [(head l)..(l !! 1)]) . map parseInt . splitOn "-") . splitOn ",") . splitOn "\n"
+
+calcPart2Alt :: String -> Integer
+calcPart2Alt = toInteger . length . filter (\(as, bs) -> any (`elem` as) bs ) . map ((\l -> (head l, l !! 1)) . map ((\l -> [(head l)..(l !! 1)]) . map parseInt . splitOn "-") . splitOn ",") . splitOn "\n"
+
 showDay :: (Integer -> Integer -> IO ()) -> String -> IO ()
 showDay printPartResult filename = do
   in_str <- readInput filename
@@ -31,3 +38,8 @@ showDay printPartResult filename = do
   printPartResult 1 $ (calcPart1 . parseInput) in_str
   -- Part 2
   printPartResult 2 $ (calcPart2 . parseInput) in_str
+  print "One-liners!"
+  -- Part 1
+  printPartResult 1 $ calcPart1Alt in_str
+  -- Part 2
+  printPartResult 2 $ calcPart2Alt in_str
