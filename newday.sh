9d1ecb0e2ceadc10ae99becba61b38c5753c1286
Tom Emelko <tom.emelko@gmail.com>
2022-12-08 21:44:29 +0000

    Update newday with a template haskell file

--- newday.sh
+++ newday.sh
@@ -2,3 +2,4 @@
 
 mkdir ./AoC$2/inputs/Day$1/
 touch ./AoC$2/inputs/Day$1/input.txt ./AoC$2/inputs/Day$1/input_simple.txt ./AoC$2/src/AoC$2Day$1.hs
+echo "module AoC$2Day$1( showDay ) where\n\nimport Util\n\nshowDay :: (Integer -> Integer -> IO ()) -> String -> IO ()\nshowDay printPartResult filename = do\n  in_str <- readInput filename" > ./AoC$2/src/AoC$2Day$1.hs
