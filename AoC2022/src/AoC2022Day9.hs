13c0c287aab5f313ebddd46203c2c0b5db27fc60
Tom Emelko <tom.emelko@gmail.com>
2022-12-09 19:36:33 +0000

    Refactor day 9

--- AoC2022/src/AoC2022Day9.hs
+++ AoC2022/src/AoC2022Day9.hs
@@ -1,15 +1,10 @@
 module AoC2022Day9( showDay ) where
 
-import Data.Set (Set)
 import Util
 import qualified Data.Set as Set
 
 data Move = Move { direction :: Char, amount :: Integer } deriving Show
 type Point = (Integer, Integer)
-
--- 3-tuple helpers
-trd3 :: (a, b, c) -> c
-trd3 (_, _, c) = c
 
 parseInput :: String -> [Move]
 parseInput = map parseMove . lines where
@@ -19,48 +14,39 @@ parseInput = map parseMove . lines where
 decAmount :: Move -> Move
 decAmount m = Move { direction = direction m, amount = amount m - 1}
 
-moveHeadOne :: (Point, Point, Set Point) -> Char -> (Point, Point, Set Point) 
-moveHeadOne ((headX, headY), tailPos, tailVisited) dir = case dir of
-  'U' -> ((headX, headY + 1), tailPos, tailVisited)
-  'D' -> ((headX, headY - 1), tailPos, tailVisited)
-  'L' -> ((headX - 1, headY), tailPos, tailVisited)
-  'R' -> ((headX + 1, headY), tailPos, tailVisited)
-  _ -> ((headX, headY), tailPos, tailVisited)
-
 diffGt1 :: Integer -> Integer -> Bool
 diffGt1 x y = abs (x - y) > 1
 
 needsMove :: Point -> Point -> Bool
 needsMove (headX, headY) (tailX, tailY) = diffGt1 headX tailX || diffGt1 headY tailY
 
-doMoveTail :: (Point, Point, Set Point) -> (Point, Point, Set Point)
-doMoveTail ((headX, headY), (tailX, tailY), tailVisited) = ((headX, headY), newTailPos, Set.insert newTailPos tailVisited) where
-  newTailPos :: Point
-  newTailPos = (tailX + signum (headX - tailX), tailY + signum (headY - tailY))
+headPath :: [Move] -> [Point]
+headPath = reverse . fst . foldl update ([(0, 0)], (0, 0)) where
+  update :: ([Point], Point) -> Move -> ([Point], Point)
+  update (ps, currentPos@(headX, headY)) move
+    | amount move == 0 = (ps, currentPos)
+    | otherwise = case direction move of
+      'U' -> update ((headX, headY + 1) : ps, (headX, headY + 1)) (decAmount move)
+      'D' -> update ((headX, headY - 1) : ps, (headX, headY - 1)) (decAmount move)
+      'L' -> update ((headX - 1, headY) : ps, (headX - 1, headY)) (decAmount move)
+      'R' -> update ((headX + 1, headY) : ps, (headX + 1, headY)) (decAmount move)
 
-moveTailOne :: (Point, Point, Set Point) -> (Point, Point, Set Point)
-moveTailOne (headPos, tailPos, tailVisited)
-  | needsMove headPos tailPos = doMoveTail (headPos, tailPos, tailVisited)
-  | otherwise = (headPos, tailPos, tailVisited)
-
-moveOne :: (Point, Point, Set Point) -> Char -> (Point, Point, Set Point)
-moveOne state = moveTailOne . moveHeadOne state
-
-doMove :: (Point, Point, Set Point) -> Move -> (Point, Point, Set Point)
-doMove (headPos, tailPos, tailVisited) move
-  | amount move == 0 = (headPos, tailPos, tailVisited)
-  | otherwise = doMove (moveOne (headPos, tailPos, tailVisited) (direction move)) (decAmount move)
-
-doMoves :: [Move] -> (Point, Point, Set Point)
-doMoves = foldl doMove ((0, 0), (0, 0), Set.fromList [(0, 0)])
+-- Given head path find tail path
+tailPath :: [Point] -> [Point]
+tailPath = reverse . foldl update []
+  where
+    update :: [Point] -> Point -> [Point]
+    update [] v = [v]
+    update path@(tailPos@(tailX, tailY):_) headPos@(headX, headY)
+      | needsMove headPos tailPos = (tailX + signum (headX - tailX), tailY + signum (headY - tailY)) : path
+      | otherwise = path
 
 calcPart1 :: [Move] -> Int
-calcPart1 = Set.size . trd3 . doMoves
+calcPart1 = Set.size . Set.fromList . tailPath . headPath
 
 showDay :: (Integer -> Int -> IO ()) -> String -> IO ()
 showDay printPartResult filename = do
   in_str <- readInput filename
-  -- print $ (doMoves . parseInput) in_str
   -- Part 1 
   printPartResult 1 $ (calcPart1 . parseInput) in_str
   -- Part 2
