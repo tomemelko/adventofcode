d309eeec2676b2d37ed1cdcadb464f22297ba9e9
Tom Emelko <tom.emelko@gmail.com>
2022-12-09 02:21:06 +0000

    Solve day 8 part 2

--- AoC2022/src/AoC2022Day8.hs
+++ AoC2022/src/AoC2022Day8.hs
@@ -3,6 +3,7 @@ module AoC2022Day8( showDay ) where
 import Util
 
 type Grid = [[Integer]]
+type Point = (Int, Int)
 
 parseInput :: String -> Grid
 parseInput = map (map parseIntChar) . lines
@@ -32,6 +33,40 @@ countVisible = fst . foldl f ([], -1) where
   f :: ([Bool], Integer) -> Integer -> ([Bool], Integer)
   f (bs, currentMax) next = ((next > currentMax) : bs, if next > currentMax then next else currentMax)
 
+addPt :: Point -> Point -> Point
+addPt (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+getValueAt :: Point -> Grid -> Integer
+getValueAt p g = (g !! snd p) !! fst p
+
+viewingDistanceInDirection :: (Point -> Point) -> (Point -> Bool) -> Point -> Grid -> Int
+viewingDistanceInDirection inc atLimit startPoint g = go (getValueAt startPoint g) (inc startPoint)
+  where
+    go :: Integer -> Point -> Int
+    go referenceHeight p
+      | atLimit p = 0
+      | getValueAt p g >= referenceHeight = 1
+      | otherwise = 1 + go referenceHeight (inc p)
+
+viewingDistance :: Grid -> Point -> Int
+viewingDistance g p = product [
+    -- Count down
+    viewingDistanceInDirection (addPt (0, 1))  (\(_, y) -> y >= length g)          p g,
+    -- Count up
+    viewingDistanceInDirection (addPt (0, -1)) (\(_, y) -> y < 0)                  p g,
+    -- Count right
+    viewingDistanceInDirection (addPt (1, 0))  (\(x, _) -> x >= (length . head) g) p g,
+    -- Count left
+    viewingDistanceInDirection (addPt (-1, 0)) (\(x, _) -> x < 0)                  p g
+  ]
+
+createPointArray :: Int -> Int -> [[Point]]
+createPointArray maxX maxY = go 0 where
+  go :: Int -> [[Point]]
+  go y
+    | y == maxY = []
+    | otherwise = [(x, y) | x <- [0..(maxX - 1)]] : go (y + 1)
+
 foldGrids :: [[[Bool]]] -> [[Bool]]
 foldGrids = foldl gridOr []
 
@@ -39,12 +74,16 @@ countTrues :: [[Bool]] -> Int
 countTrues = sum . map (length . filter id)
 
 calcPart1 :: Grid -> Int
-calcPart1 = countTrues . foldGrids . deRotations . map (map countVisible) . rotations 
+calcPart1 = countTrues . foldGrids . deRotations . map (map countVisible) . rotations
+
+calcPart2 :: Grid -> Int
+calcPart2 g = (maximum . map (maximum . map (viewingDistance g))) (createPointArray ((length . head) g) (length g))
 
 showDay :: (Integer -> Int -> IO ()) -> String -> IO ()
 showDay printPartResult filename = do
   in_str <- readInput filename
-  -- Part 1
+  -- print $ (viewingDistance . parseInput) in_str
+  -- Part 1 
   printPartResult 1 $ (calcPart1 . parseInput) in_str
   -- Part 2
-  -- printPartResult 2 $ (calcPart2 . parseInput) in_str
+  printPartResult 2 $ (calcPart2 . parseInput) in_str
