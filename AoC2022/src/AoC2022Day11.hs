42d11174bc65fb7406138403125ae0373599b822
Tom Emelko <tom.emelko@gmail.com>
2022-12-11 07:22:23 +0000

    Solve day 11 part 2
    I had to switch everything to Integers because Int was silently overflowing

--- AoC2022/src/AoC2022Day11.hs
+++ AoC2022/src/AoC2022Day11.hs
@@ -4,57 +4,66 @@ import Data.List
 import Data.List.Split
 import Util
 
-data Monkey = Monkey { items :: [Int], operation :: Int -> Int, test :: Int -> Int, inspectCount :: Int }
+data Monkey = Monkey { items :: [Integer], operation :: Integer -> Integer, test :: Integer -> Integer, inspectCount :: Integer, divisor :: Integer }
 instance Show Monkey where
   show m = (show . items) m ++ " with seeing " ++ (show . inspectCount) m
 
+(!!!) :: [a] -> Integer -> a
+(!!!) (x : _) 0 = x
+(!!!) (_ : xs) n = (!!!) xs (n - 1)
+
 parseMonkey :: String -> Monkey
-parseMonkey s = Monkey { items = getItems splitted, operation = getOperation splitted, test = getTest splitted, inspectCount = 0 } where
+parseMonkey s = Monkey { items = getItems splitted, operation = getOperation splitted, test = getTest (divCheck splitted) splitted, inspectCount = 0, divisor = divCheck splitted} where
   splitted = lines s
-  getItems :: [String] -> [Int]
+  getItems :: [String] -> [Integer]
   getItems = map read . splitOn "," . filter (/=' ') . (!! 1) . splitOn ":" . (!! 1)
-  getOperation :: [String] -> (Int -> Int)
+  getOperation :: [String] -> (Integer -> Integer)
   getOperation = buildFunc . filter (/=' ') . (!! 1) . splitOn "=" . (!! 2) where
-    buildFunc :: String -> (Int -> Int)
+    buildFunc :: String -> (Integer -> Integer)
     buildFunc opLine = if operand == "old" then (\x -> x `operator` x) else operator (read operand) where
       operator = if '+' `elem` opLine then (+) else (*)
       operand = ((!! 1) . splitWhen (`elem` "+*")) opLine
-  getTest :: [String] -> (Int -> Int)
-  getTest ss = \x -> if x `mod` divCheck == 0 then parseTarget (ss !! 4) else parseTarget (ss !! 5) where
-    divCheck :: Int
-    divCheck = (read . (!! 3) . splitOn " " . drop 2 . (!! 3)) ss
-    parseTarget :: String -> Int
+  divCheck :: [String] -> Integer
+  divCheck ss = (read . (!! 3) . splitOn " " . drop 2 . (!! 3)) ss
+  getTest :: Integer -> [String] -> (Integer -> Integer)
+  getTest divCheck' ss = \x -> if x `mod` divCheck' == 0 then parseTarget (ss !! 4) else parseTarget (ss !! 5) where
+    parseTarget :: String -> Integer
     parseTarget = read . (!! 5) . splitOn " " . drop 4
 
 parseInput :: String -> [Monkey]
 parseInput = map parseMonkey . splitOn "\n\n"
 
-updateInspected :: [Monkey] -> Int -> [Monkey]
-updateInspected (m : ms) 0 = Monkey { items = [], operation = operation m, test = test m, inspectCount = inspectCount m + length (items m) } : ms
+updateInspected :: [Monkey] -> Integer -> [Monkey]
+updateInspected (m : ms) 0 = Monkey { items = [], operation = operation m, test = test m, inspectCount = inspectCount m + (toInteger . length . items) m, divisor = divisor m } : ms
 updateInspected (m : ms) i = m : updateInspected ms (i - 1)
 
-updateReceivers :: [Monkey] -> [(Int, Int)] -> [Monkey]
+updateReceivers :: [Monkey] -> [(Integer, Integer)] -> [Monkey]
 updateReceivers = foldl updateReceiver where
-  updateReceiver :: [Monkey] -> (Int, Int) -> [Monkey]
-  updateReceiver (m : ms') (thrownItem, 0) = Monkey { items = items m `append` thrownItem, operation = operation m, test = test m, inspectCount = inspectCount m } : ms'
+  updateReceiver :: [Monkey] -> (Integer, Integer) -> [Monkey]
+  updateReceiver (m : ms') (thrownItem, 0) = Monkey { items = items m `append` thrownItem, operation = operation m, test = test m, inspectCount = inspectCount m, divisor = divisor m } : ms'
   updateReceiver (m : ms') (thrownItem, target) = m : updateReceiver ms' (thrownItem, target - 1)
 
-takeMonkeyTurn :: [Monkey] -> Int -> [Monkey]
-takeMonkeyTurn ms i = updateReceivers (updateInspected ms i) throws where 
-  throws = map ((\x -> (x, test monkey x)) . (`div` 3) . operation monkey) (items monkey) where
+takeMonkeyTurn :: (Integer -> Integer) -> [Monkey] -> Integer -> [Monkey]
+takeMonkeyTurn worryLevelMod ms i = updateReceivers (updateInspected ms i) throws where 
+  throws = map ((\x -> (x, test monkey x)) . worryLevelMod . operation monkey) (items monkey) where
     monkey :: Monkey
-    monkey = ms !! i
+    monkey = ms !!! i
 
-doRound :: [Monkey] -> [Monkey]
-doRound ms = foldl takeMonkeyTurn ms [0..(length ms - 1)]
+doRound :: (Integer -> Integer) -> [Monkey] -> [Monkey]
+doRound worryLevelMod ms = foldl (takeMonkeyTurn worryLevelMod) ms [0..((toInteger . length) ms - 1)]
 
-calcPart1 :: [Monkey] -> Int
-calcPart1 = product . take 2 . reverse . sort . map inspectCount . (!! 20) . iterate doRound
+calcPart1 :: [Monkey] -> Integer
+calcPart1 = product . take 2 . reverse . sort . map inspectCount . (!! 20) . iterate (doRound (`div` 3))
 
-showDay :: (Integer -> Int -> IO ()) -> String -> IO ()
-showDay printPartResult filename = do
+calcPart2 :: [Monkey] -> Integer
+calcPart2 ms = (product . take 2 . reverse . sort . map inspectCount . (!! 10000) . iterate (doRound (`mod` productOfDivisors))) ms where
+  productOfDivisors :: Integer
+  productOfDivisors = (product . map divisor) ms
+
+showDay :: (Integer -> Integer -> IO ()) -> String -> IO ()
+showDay prIntegerPartResult filename = do
   in_str <- readInput filename
   -- Part 1 
-  printPartResult 1 $ (calcPart1 . parseInput) in_str
+  prIntegerPartResult 1 $ (calcPart1 . parseInput) in_str
   -- Part 2
-  -- printPartResult 2 $ (calcPart2 . parseInput) in_str
+  prIntegerPartResult 2 $ (calcPart2 . parseInput) in_str
