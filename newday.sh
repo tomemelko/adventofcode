ef587afaeda8a6e4d510ed69955a6df2e1002e43
Tom Emelko <tom.emelko@gmail.com>
2022-12-13 18:00:30 +0000

    Add part1/part2 templates tp newday.sh

--- newday.sh
+++ newday.sh
@@ -2,4 +2,4 @@
 
 mkdir ./AoC$2/inputs/Day$1/
 touch ./AoC$2/inputs/Day$1/input.txt ./AoC$2/inputs/Day$1/input_simple.txt ./AoC$2/src/AoC$2Day$1.hs
-echo "module AoC$2Day$1( showDay ) where\n\nimport Util\n\nshowDay :: (Integer -> Integer -> IO ()) -> String -> IO ()\nshowDay printPartResult filename = do\n  in_str <- readInput filename" > ./AoC$2/src/AoC$2Day$1.hs
+echo "module AoC$2Day$1( showDay ) where\n\nimport Util\n\nshowDay :: (Integer -> Integer -> IO ()) -> String -> IO ()\nshowDay printPartResult filename = do\n  in_str <- readInput filename\n  -- Part 1\n  -- printPartResult 1 $ (calcPart1 . parseInput) in_str\n  -- Part 2\n  -- printPartResult 2 $ (calcPart2 . parseInput) in_str" > ./AoC$2/src/AoC$2Day$1.hs
