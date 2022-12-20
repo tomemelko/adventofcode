0353ffd091666d2464555d379de33e07f9d9b595
Tom Emelko <tom.emelko@gmail.com>
2022-12-20 21:46:27 +0000

    Make some perf improvements to day 17 part 1

--- AoC2022/src/AoC2022Day17.hs
+++ AoC2022/src/AoC2022Day17.hs
@@ -1,11 +1,9 @@
 module AoC2022Day17( showDay ) where
 
 import Debug.Trace
-import Data.Map (Map)
 import Data.Set (Set)
 import Util
 
-import qualified Data.Map as Map
 import qualified Data.Set as Set
 
 data WindDirection = BlowLeft | BlowRight deriving Show
@@ -13,13 +11,10 @@ type Point = (Int, Int)
 type Rock = [Point]
 
 parseInput :: String -> [WindDirection]
-parseInput = map (\c -> if c == '<' then BlowLeft else BlowRight)
-
-modIndex :: [a] -> Int -> a
-modIndex xs i = xs !! (i `mod` length xs)
+parseInput = cycle . map (\c -> if c == '<' then BlowLeft else BlowRight)
 
 shapes :: [Rock]
-shapes = [
+shapes = cycle [
     -- ####
     [(0,0), (1,0), (2,0), (3,0)], 
     -- .#.
@@ -48,28 +43,27 @@ blowRock r d = case d of
 dropRock :: Rock -> Rock
 dropRock = map (\(x, y) -> (x, y-1))
 
-spawnRock :: Set Point -> Rock -> Rock
-spawnRock board = map (\(x,y) -> (x + 2, y + maxY + 4)) where
-  maxY :: Int
-  maxY = (maximum . map snd . Set.toList) board
+spawnRock :: Int -> Rock -> Rock
+spawnRock maxY = map (\(x,y) -> (x + 2, y + maxY + 4))
 
-fallRock :: [WindDirection] -> (Set Point, Int) -> Rock -> (Set Point, Int)
-fallRock blows (board, blowsIdx) rock
+fallRock :: ([WindDirection], Set Point, Int) -> Rock -> ([WindDirection], Set Point, Int)
+fallRock (blows, board, maxY) rock
   -- Hits left or right wall or blow collides with existing board, only drop the rock
-  | any (\p@(x, _) -> x < 0 || x > 6 || Set.member p board) (blowRock rock (blows `modIndex` blowsIdx)) = doDrop rock
+  | any (\p@(x, _) -> x < 0 || x > 6 || Set.member p board) (blowRock rock (head blows)) = doDrop rock
   -- Otherwise
-  | otherwise = doDrop (blowRock rock (blows `modIndex` blowsIdx)) where
-    doDrop :: Rock -> (Set Point, Int)
+  | otherwise = doDrop (blowRock rock (head blows)) where
+    doDrop :: Rock -> ([WindDirection], Set Point, Int)
     doDrop rock'
       -- Hit bottom
-      | any (`Set.member` board) (dropRock rock') = (foldr Set.insert board rock', blowsIdx + 1)
-      | otherwise = fallRock blows (board, blowsIdx + 1) (dropRock rock')
+      | any (`Set.member` board) dropped = (tail blows, foldr Set.insert board rock', maximum (maxY : map snd rock))
+      | otherwise = fallRock (tail blows, board, maxY) dropped where
+        dropped = dropRock rock'
 
-fallRocks :: Int -> [WindDirection] -> (Set Point, Int)
-fallRocks count blows = (foldl (\acc@(board, _) nextRock -> fallRock blows acc (spawnRock board nextRock)) (Set.fromList [(0,0), (1,0), (2,0), (3,0), (4,0), (5,0), (6,0)], 0) . map (\x -> shapes `modIndex` x)) [0..(count - 1)]
+fallRocks :: Int -> [WindDirection] -> ([WindDirection], Set Point, Int)
+fallRocks count blows = (foldl (\acc@(_, _, maxY) nextRock -> fallRock acc (spawnRock maxY nextRock)) (blows, Set.fromList [(0,0), (1,0), (2,0), (3,0), (4,0), (5,0), (6,0)], 0) . take count) shapes
 
 calcPart1 :: [WindDirection] -> Int
-calcPart1 = maximum . map snd . Set.toList . fst . fallRocks 2022
+calcPart1 = thd3 . fallRocks 2022
 
 showDay :: (Integer -> Int -> IO ()) -> String -> IO ()
 showDay printPartResult filename = do
