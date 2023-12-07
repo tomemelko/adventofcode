38f40a743ebabb71b1fe68606bc72b08408e6506
Tom Emelko <tom.emelko@gmail.com>
2023-12-06 22:49:20 -0500

    Solve day 4 part 2

--- AoC2023/src/day4.rs
+++ AoC2023/src/day4.rs
@@ -1,9 +1,9 @@
 use regex::Regex;
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 
 #[derive(Debug)]
 struct Card {
-    _n: i32,
+    n: i32,
     winners: HashSet<i32>,
     have: HashSet<i32>,
 }
@@ -20,31 +20,44 @@ fn parse_line(line: &str) -> Card {
         have.insert(group.get(0).unwrap().as_str().parse::<i32>().unwrap());
     }
     return Card {
-        _n: Regex::new(r"\d+").unwrap().captures(c).unwrap().get(0).unwrap().as_str().parse::<i32>().unwrap(),
+        n: Regex::new(r"\d+")
+            .unwrap()
+            .captures(c)
+            .unwrap()
+            .get(0)
+            .unwrap()
+            .as_str()
+            .parse::<i32>()
+            .unwrap(),
         winners: winners.to_owned(),
         have: have.to_owned(),
     };
 }
 
-fn part1(raw_data: &str) -> i32 {
-    return raw_data
-        .split("\n")
-        .map(|x| {
-            let c = parse_line(x);
+fn get_sums(raw_data: &str) -> (i32, i32) {
+    let mut part1 = 0;
+    let mut part2 = 0;
+    let mut counts = HashMap::new();
+    for row in raw_data.split("\n") {
+            let c = parse_line(row);
             let my_winners = c.winners.intersection(&c.have).count() as i32;
             let score = match my_winners.cmp(&0) {
-              std::cmp::Ordering::Greater => 2_i32.pow((my_winners - 1) as u32),
-              _ => 0
+                std::cmp::Ordering::Greater => 2_i32.pow((my_winners - 1) as u32),
+                _ => 0,
             };
             // println!("{:?} ({}): {:?}", my_winners, score, c);
-            return score
-        })
-        .sum();
+            part1 += score;
+            let card_count: i32 = counts.get(&c.n).unwrap_or(&1).to_owned();
+            part2 += card_count;
+            for next_g in (c.n+1)..(c.n+my_winners+1) {
+              counts.insert(next_g, counts.get(&next_g).unwrap_or(&1) + card_count);
+            }
+        };
+    return (part1, part2);
 }
 
 pub fn day4(raw_data: &str) {
-    let p1sum = part1(raw_data);
-    let p2sum: i32 = 0;
+    let (p1sum, p2sum) = get_sums(raw_data);
     println!("Part 1 result: {p1sum:?}");
     println!("Part 2 result: {p2sum:?}");
 }
